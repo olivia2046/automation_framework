9a0dc7c8b9f8d53cdaa7b091f02effa2f36db86c
Olivia Dou <olivia2046@hotmail.com>
2026-03-10 15:07:04 +0800

    include trend in allure report

--- allure-report.bat
+++ allure-report.bat
@@ -1,1 +1,3 @@
-allure serve testreport/xml
+xcopy testreport\allure-report\history testreport\allure-results\history /E /I /Y
+start allure generate testreport\allure-results --clean -o testreport\allure-report
+start cmd /k allure serve testreport\allure-results
