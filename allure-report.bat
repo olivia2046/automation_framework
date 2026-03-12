3c7609e1005b1fa1d6d730a040baae33f3731139
Olivia Dou <olivia2046@hotmail.com>
2026-03-12 09:53:14 +0800

    add gitignore

--- allure-report.bat
+++ allure-report.bat
@@ -1,3 +1,4 @@
+rem Used after test execution to generate single file allure report with history trend
 xcopy testreport\allure-report\history testreport\allure-results\history /E /I /Y
 rem start allure generate testreport\allure-results --clean -o testreport\allure-report
 rem start cmd /k allure serve testreport\allure-results
